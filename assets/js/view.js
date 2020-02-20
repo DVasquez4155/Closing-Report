112eed1cf44899efcdb8abff5c74de220b9ec82d
Daniel V <main@dvasquez4155.com>
2020-02-19 16:36:03 -0800

    Create view.html & view.js
    You can now view previous entries of reports

---
+++ assets/js/view.js
@@ -0,0 +1,26 @@
+var values = getValues('closeReport');
+var report = document.calculator;
+var hash = parseInt(location.hash.substring(1));
+function fill(shiftNumber){
+    if (!isNaN(shiftNumber)) {
+        var shift = values[shiftNumber];
+        report.name.value = shift.name;
+        report.station.value = shift.station;
+        report.date.value = shift.date;
+        report.shift.value = shift.shift;
+        report.tipCharge.value = shift.tipCharge;
+        report.chargeTip.value = shift.chargeTip;
+        report.otherSales.value = shift.otherSales;
+        report.cashTips.value = shift.cashTips;
+        report.barSales.value = shift.barSales;
+        calculator(document.calculator)
+    }
+}
+fill(hash);
+for (i = 0; i < report.length - 1; i++) {
+    $(report[i]).attr('disabled', true)
+    report.bar
+}
+$('#return').on('click', function(e) {
+    window.location.href = 'index.html';
+})
