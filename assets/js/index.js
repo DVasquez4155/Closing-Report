23dca58ad7c334f22f361c11b79823e5d3e6355c
Daniel V <main@dvasquez4155.com>
2020-02-19 16:35:24 -0800

    Create index.js
    Checks to see if there are entries of the report

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+$('#new').on('click', function() {
+    window.location.href = 'calculator.html';
+});
+var values = getValues('closeReport');
+var cardList = $('#list')
+$(values).each(function(i) {
+    var date = moment(values[i].date);
+    if ($('#' + date.format('MMM_YYYY')).length == 0) {
+        cardList.append(
+            '<div class="card-header">' +
+            '<button class="btn btn-link" data-toggle="collapse" data-target="#' + date.format('MMM_YYYY') + '">' +
+            date.format('MMMM') +
+            '</button>' +
+            '</div>')
+        .append(
+            '<div id="' + date.format('MMM_YYYY') + '" class="collapse">' +
+            '<ul id="' + date.format('MMM_YYYY') + 'ul" class="list-group">' +
+            '</ul>' +
+            '</div>')
+    }
+    // if ($('#' + date.format('MMM_YYYY')))
+    var list = document.createElement('li');
+    $(list)
+    .addClass('list-group-item')
+    .html('<a href="view.html#' + i + '">' + date.format('M/DD/YYYY') + "</a>")
+    .appendTo($('#' + date.format('MMM_YYYY') + 'ul'));
+})
